d5e65f2c7012df733611a8eb10dde63d7805610e
Yash Gupta <ygup098@gmail.com>
2026-07-06 11:54:48 -0700

    COMMIT

--- backend/server.js
+++ backend/server.js
@@ -73,8 +73,6 @@ app.get("/models", async (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => {
-
-  console.log(`Server running on ${PORT}`);
-
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 Server running on port ${PORT}`);
 });
